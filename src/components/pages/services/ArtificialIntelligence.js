38c32c53dfb48615fcb3d5ca16c3bb7ce818ff83
param <paramug2@gmail.com>
2021-08-22 20:56:32 +0530

    Services inital start

--- src/components/pages/services/ArtificialIntelligence.js
+++ src/components/pages/services/ArtificialIntelligence.js
@@ -2,7 +2,395 @@ import React, { Fragment } from 'react';
 
 const ArtificialIntelligence = () => {
   return (
-    <Fragment><h1>ArtificialIntelligence</h1></Fragment>
-  )
-}
+    <Fragment>
+      <div className="banner_area banner3 d-flex align-items-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="single_banner">
+                <div className="single_banner_content">
+                  <div className="banner_text_content white">
+                    <h5 className="mb-3 wow fadeInUp" data-wow-delay="0.5s">
+                      Welcome To Techno
+                    </h5>
+                    <h1 className="wow fadeInUp" data-wow-delay="0.4s">
+                      Affiliate Intelligent
+                    </h1>
+                    <h1 className="wow fadeInUp" data-wow-delay="0.4s">
+                      Products Creators
+                    </h1>
+                  </div>
+                  <div className="banner_content_text white pt-30">
+                    <p className="wow fadeInUp" data-wow-delay="0.5s">
+                      Techno real-time data management technologies, global data
+                      science, and award-winning customer service make our
+                      unstacked data solutions dreamit authors team member.
+                    </p>
+                  </div>
+                  <div className="slider_button pt-25 d-flex">
+                    <div className="button color_two">
+                      <a
+                        className="wow fadeInUp"
+                        data-wow-delay="0.5s"
+                        href="#"
+                      >
+                        How IT Work <i className="fa fa-long-arrow-right"></i>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="data_science_video white-color">
+                    <div
+                      className="data_science_video_inner wow fadeInUp"
+                      data-wow-delay="0.5s"
+                    >
+                      <a
+                        className="video-vemo-icon venobox vbox-item"
+                        data-vbtype="youtube"
+                        data-autoplay="true"
+                        href="https://youtu.be/BS4TUd7FJSg"
+                      >
+                        <i className="fa fa-play"></i>
+                        <div id="spinnerbtn"></div>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="single_banner_thumb">
+                <div className="affiliate_banner_shape">
+                  <div
+                    className="affiliate_banner_shape_inner1 wow fadeInUp"
+                    data-wow-delay="0.5s"
+                  >
+                    <img src="assets/images/shape/af1.png" alt="" />
+                  </div>
+                  <div className="affiliate_banner_shape_inner2 bounce-animate2">
+                    <img src="assets/images/shape/af2.png" alt="" />
+                  </div>
+                  <div
+                    className="affiliate_banner_shape_inner3 wow fadeInUp"
+                    data-wow-delay="0.5s"
+                  >
+                    <img src="assets/images/shape/af3.png" alt="" />
+                  </div>
+                  <div className="affiliate_banner_shape_inner4 rotateme">
+                    <img src="assets/images/shape/bnr.png" alt="" />
+                  </div>
+                  <div
+                    className="affiliate_banner_shape_inner5 wow fadeInUp"
+                    data-wow-delay="0.5s"
+                  >
+                    <img src="assets/images/shape/af5.png" alt="" />
+                  </div>
+                  <div className="affiliate_banner_shape_inner6 bounce-animate3">
+                    <img src="assets/images/shape/3.png" alt="" />
+                  </div>
+                  <div className="affiliate_banner_shape_inner7 rotateme">
+                    <img src="assets/images/shape/rt1.png" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="process_area bg_color2 pt-90 pb-70">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div
+                className="section_title text-center mb-50 wow fadeInDown"
+                data-wow-delay="0.3s"
+              >
+                <div className="section_sub_title uppercase mb-3">
+                  <h6>Our Working Process</h6>
+                </div>
+                <div className="section_main_title">
+                  <h1>Software Testing Process</h1>
+                  <h1>
+                    For My <span>Company</span>
+                  </h1>
+                </div>
+                <div className="em_bar">
+                  <div className="em_bar_bg"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div
+                className="process_style_two text-center mb-30 wow fadeInUp"
+                data-wow-delay="0.3s"
+              >
+                <div className="process_style_two_thumb">
+                  <img src="assets/images/new/pr1.png" alt="" />
+                </div>
+                <div className="process_style_two_content">
+                  <div className="process_style_two_content_title pt-3 pb-2">
+                    <h5>Company Planning</h5>
+                  </div>
+                  <div className="process_style_two_content_text">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore
+                    </p>
+                  </div>
+                  <div className="process_style_two_content_button">
+                    <a href="#">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div
+                className="process_style_two text-center mb-30 wow fadeInUp"
+                data-wow-delay="0.3s"
+              >
+                <div className="process_style_two_thumb">
+                  <img src="assets/images/new/pr2.png" alt="" />
+                </div>
+                <div className="process_style_two_content">
+                  <div className="process_style_two_content_title pt-3 pb-2">
+                    <h5>Design & Execution</h5>
+                  </div>
+                  <div className="process_style_two_content_text">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore
+                    </p>
+                  </div>
+                  <div className="process_style_two_content_button">
+                    <a href="#">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div
+                className="process_style_two text-center mb-30 wow fadeInUp"
+                data-wow-delay="0.3s"
+              >
+                <div className="process_style_two_thumb">
+                  <img src="assets/images/new/pr3.png" alt="" />
+                </div>
+                <div className="process_style_two_content">
+                  <div className="process_style_two_content_title pt-3 pb-2">
+                    <h5>Analytics Projects</h5>
+                  </div>
+                  <div className="process_style_two_content_text">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore
+                    </p>
+                  </div>
+                  <div className="process_style_two_content_button">
+                    <a href="#">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="service_area pt-80 pb-70">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div
+                className="section_title text-center mb-50 wow fadeInDown"
+                data-wow-delay="0.5s"
+              >
+                <div className="section_sub_title uppercase mb-3">
+                  <h6>Our Services</h6>
+                </div>
+                <div className="section_main_title">
+                  <h1>Explore Our Machine Learning</h1>
+                  <h1>For Your Best Service</h1>
+                </div>
+                <div className="em_bar">
+                  <div className="em_bar_bg"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div
+                className="service_style_eleven  wow flipInY"
+                data-wow-delay="0ms"
+                data-wow-duration="2500ms"
+              >
+                <div className="single_service_style_eleven">
+                  <div className="service_style_eleven_icon">
+                    <div className="icon">
+                      <i className="flaticon-data"></i>
+                    </div>
+                  </div>
+                  <div className="service_style_eleven_title pb-4">
+                    <h4>Big Data</h4>
+                  </div>
+                  <div className="service_style_eleven_text pt-2">
+                    <p>
+                      Lorem ipsum dolor amet, consectetur adipiscing elit, sed
+                      do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Quis ipsum
+                    </p>
+                  </div>
+                  <div className="servic_style_eleven_button">
+                    <a href="service-1.html">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div
+                className="service_style_eleven  wow flipInY"
+                data-wow-delay="0ms"
+                data-wow-duration="2500ms"
+              >
+                <div className="single_service_style_eleven">
+                  <div className="service_style_eleven_icon">
+                    <div className="icon">
+                      <i className="flaticon-system"></i>
+                    </div>
+                  </div>
+                  <div className="service_style_eleven_title pb-4">
+                    <h4>Data Analytics</h4>
+                  </div>
+                  <div className="service_style_eleven_text pt-2">
+                    <p>
+                      Lorem ipsum dolor amet, consectetur adipiscing elit, sed
+                      do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Quis ipsum
+                    </p>
+                  </div>
+                  <div className="servic_style_eleven_button">
+                    <a href="service-1.html">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div
+                className="service_style_eleven  wow flipInY"
+                data-wow-delay="0ms"
+                data-wow-duration="2500ms"
+              >
+                <div className="single_service_style_eleven">
+                  <div className="service_style_eleven_icon">
+                    <div className="icon">
+                      <i className="flaticon-padlock"></i>
+                    </div>
+                  </div>
+                  <div className="service_style_eleven_title pb-4">
+                    <h4>Managed Analytics</h4>
+                  </div>
+                  <div className="service_style_eleven_text pt-2">
+                    <p>
+                      Lorem ipsum dolor amet, consectetur adipiscing elit, sed
+                      do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Quis ipsum
+                    </p>
+                  </div>
+                  <div className="servic_style_eleven_button">
+                    <a href="service-1.html">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div
+                className="service_style_eleven  wow flipInY"
+                data-wow-delay="0ms"
+                data-wow-duration="2500ms"
+              >
+                <div className="single_service_style_eleven">
+                  <div className="service_style_eleven_icon">
+                    <div className="icon">
+                      <i className="flaticon-process-1"></i>
+                    </div>
+                  </div>
+                  <div className="service_style_eleven_title pb-4">
+                    <h4>Business Intelligence</h4>
+                  </div>
+                  <div className="service_style_eleven_text pt-2">
+                    <p>
+                      Lorem ipsum dolor amet, consectetur adipiscing elit, sed
+                      do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Quis ipsum
+                    </p>
+                  </div>
+                  <div className="servic_style_eleven_button">
+                    <a href="service-1.html">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div
+                className="service_style_eleven  wow flipInY"
+                data-wow-delay="0ms"
+                data-wow-duration="2500ms"
+              >
+                <div className="single_service_style_eleven">
+                  <div className="service_style_eleven_icon">
+                    <div className="icon">
+                      <i className="flaticon-chart"></i>
+                    </div>
+                  </div>
+                  <div className="service_style_eleven_title pb-4">
+                    <h4>Data Science</h4>
+                  </div>
+                  <div className="service_style_eleven_text pt-2">
+                    <p>
+                      Lorem ipsum dolor amet, consectetur adipiscing elit, sed
+                      do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Quis ipsum
+                    </p>
+                  </div>
+                  <div className="servic_style_eleven_button">
+                    <a href="service-1.html">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div
+                className="service_style_eleven  wow flipInY"
+                data-wow-delay="0ms"
+                data-wow-duration="2500ms"
+              >
+                <div className="single_service_style_eleven">
+                  <div className="service_style_eleven_icon">
+                    <div className="icon">
+                      <i className="flaticon-data-2"></i>
+                    </div>
+                  </div>
+                  <div className="service_style_eleven_title pb-4">
+                    <h4>Data Management</h4>
+                  </div>
+                  <div className="service_style_eleven_text pt-2">
+                    <p>
+                      Lorem ipsum dolor amet, consectetur adipiscing elit, sed
+                      do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Quis ipsum
+                    </p>
+                  </div>
+                  <div className="servic_style_eleven_button">
+                    <a href="service-1.html">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 export default ArtificialIntelligence;
