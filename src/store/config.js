2bfa92cb547e192c3a4b81f378973cf753b1a9f2
param <paramug2@gmail.com>
2021-09-12 16:32:20 +0530

    Ui update

--- src/store/config.js
+++ src/store/config.js
@@ -1,5 +1,8 @@
 const cloudinConfig = {
   REGION_API_URL: 'https://restcountries.eu/rest/v2/region',
+  SERVICE_id:'service_ozcfde7',
+  TEMPLATE_ID:'template_zykfpfb',
+  USER_ID:'user_6Es7aWUQTMMl0tEzVPdWL'
 };
 
 export default cloudinConfig;
