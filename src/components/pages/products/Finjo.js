9bd4e0e52338711c136f376354c3fa1d1de61343
param <paramug2@gmail.com>
2021-11-16 16:08:53 +0530

    banner update

--- src/components/pages/products/Finjo.js
+++ src/components/pages/products/Finjo.js
@@ -36,7 +36,7 @@ const Finjo = () => {
           <div className="row">
             <div className="col-lg-6 col-md-6 col-sm-12 col-xs-6">
               <div className="about_thumb">
-                <img src="assets/images/about-2.jpg" alt="" />
+                <img src="assets/images/finjo_side.svg" alt="" />
               </div>
             </div>
             <div className="col-lg-6 col-md-6 col-sm-12 col-xs-6">
--- src/components/pages/products/MrBusiness.js
+++ src/components/pages/products/MrBusiness.js
@@ -37,7 +37,7 @@ const MrBusiness = () => {
           <div className="row">
             <div className="col-lg-6 col-md-6 col-sm-12 col-xs-6">
               <div className="about_thumb">
-                <img src="assets/images/about-2.jpg" alt="" />
+                <img src="assets/images/mrBusi_side.svg" alt="" />
               </div>
             </div>
             <div className="col-lg-6 col-md-6 col-sm-12 col-xs-6">
