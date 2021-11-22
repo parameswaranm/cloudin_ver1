38403dab1b08a8762ae17afaf93f2371742da9aa
param <paramug2@gmail.com>
2021-11-22 13:35:31 +0530

    ui

--- src/components/pages/services/DigitalMarket.js
+++ src/components/pages/services/DigitalMarket.js
@@ -7,7 +7,7 @@ const DigitalMarket = () => {
   }, []);
   return (
     <Fragment>
-      <div className="breatcome_virtualassist d-flex align-items-center">
+      <div className="breatcome_dm d-flex align-items-center">
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
