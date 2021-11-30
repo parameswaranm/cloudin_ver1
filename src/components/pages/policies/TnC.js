e0358bc97569b8b9b407fe4a990519390fb6be50
param <paramug2@gmail.com>
2021-11-30 15:09:27 +0530

    banner update

--- src/components/pages/policies/TnC.js
+++ src/components/pages/policies/TnC.js
@@ -7,7 +7,7 @@ const TnC = () => {
   }, [])
   return (
     <Fragment>
-      <div className="breatcome_area d-flex align-items-center">
+      <div className="breatcome_policies d-flex align-items-center">
         <div className="container breadcrumb_align_lft">
           <div className="row">
             <div className="col-lg-12">
